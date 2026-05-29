c460a4366bc2bdc346a94b080d485142ea1f7758
rkyarc <arickyh1@gmail.com>
2026-05-29 22:15:19 +0700

    feat: menambah logika tombol dark mode

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const filterBtn = $('.filter-btn');
 const katalogItems = $('.catalog-item');
 
+let currentTheme = localStorage.getItem('theme') || 'light';
+$('html').attr('data-bs-theme', currentTheme);
+
+if (currentTheme === 'dark') {
+  $('#themeIcon').removeClass('bi-moon-fill').addClass('bi-sun-fill');
+  $('#themeToggle').removeClass('btn-outline-dark').addClass('btn-outline-light');
+}
+
+$('#themeToggle').click(function () {
+  let theme = $('html').attr('data-bs-theme');
+  
+  let newTheme = (theme === 'dark') ? 'light' : 'dark';
+  
+  $('html').attr('data-bs-theme', newTheme);
+  
+  localStorage.setItem('theme', newTheme);
+  
+  $('#themeIcon').toggleClass('bi-moon-fill bi-sun-fill');
+  $('#themeToggle').toggleClass('btn-outline-dark btn-outline-light');
+});
+
 filterBtn.click(function () {
 
     filterBtn.removeClass('active');
